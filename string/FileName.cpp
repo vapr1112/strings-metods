847a88f488c25b987c115544029c769dae50e0b3
307k7 <307k7@DESKTOP-2P9B4M4>
2024-12-10 18:37:38 +0300

    Добавьте файлы проекта.

---
+++ string/FileName.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+
+	string str_1, str_2;
+
+	str_1 = "Hello, Ivan";
+
+	cout << "\n¬ведите \n";
+
+	return 0;
+}
